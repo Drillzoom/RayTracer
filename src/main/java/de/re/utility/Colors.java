6d165268aabe03f87ba718433dfdd7a35d1c4ca5
Raffael E <raffa_e@web.de>
2020-12-05 15:40:31 +0100

    added a simple red sphere

--- src/main/java/de/re/utility/Colors.java
+++ src/main/java/de/re/utility/Colors.java
@@ -1,6 +1,7 @@
 package de.re.utility;
 
 import de.re.common.Color;
+import de.re.common.Point3;
 import de.re.common.Vec3;
 import de.re.model.Ray;
 
@@ -21,10 +22,23 @@ public final class Colors {
     }
 
     public static Color rayColor(Ray r) {
+        if (hitSphere(new Point3(0.0f, 0.0f, -1.0f), 0.5f, r)) {
+            return new Color(1.0f, 0.0f, 0.0f);
+        }
+
         Vec3 unitDirection = Vectors.unitVector(r.direction);
         float t = 0.5f * (unitDirection.y() + 1.0f);
         Color c1 = (Color) new Color(1.0f, 1.0f, 1.0f).mul(1.0f - t);
         Color c2 = (Color) new Color(0.5f, 0.7f, 1.0f).mul(t);
         return (Color) c1.add(c2);
+    }
+
+    private static boolean hitSphere(Point3 center, float radius, Ray r) {
+        Vec3 oc = Vectors.sub(r.origin, center);
+        float a = Vectors.dot(r.direction, r.direction);
+        float b = 2.0f * Vectors.dot(oc, r.direction);
+        float c = Vectors.dot(oc, oc) - radius*radius;
+        float discriminant = b*b - 4*a*c;
+        return discriminant > 0;
     }
 }
